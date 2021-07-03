96a1e37bbec7516010ee197472a6f6a020c85a67
sree0201 <69725809+sree0201@users.noreply.github.com>
2021-07-03 10:26:08 +0530

    Create Assignment_1.py

---
+++ Python/Assignment_1.py
@@ -0,0 +1,15 @@
+#Assignment 1
+#Caluclator added with subtraction, multiplication, divison.
+
+a=int(input("Enter the number a: "))
+b=int(input("Enter the number b: "))
+
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+
+print("addition",add)
+print("Subtraction",sub)
+print("Multiplication",mul)
+print("Divison",div)
